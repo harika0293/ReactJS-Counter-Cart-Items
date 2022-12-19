ba662dded4df5b89cc88dddeb8dd19993153e16c
Harika Rachamalla <harika.0293@gmail.com>
2022-12-19 18:25:12 +0530

    Updates

---
+++ precache-manifest.684ccc6619eb717e4bad8d89da0ab02a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1cb40b715954e12a224",
+    "url": "/ReactJS-Counter-Cart-Items/static/css/main.4998dd3e.chunk.css"
+  },
+  {
+    "revision": "f1cb40b715954e12a224",
+    "url": "/ReactJS-Counter-Cart-Items/static/js/main.0a7d308d.chunk.js"
+  },
+  {
+    "revision": "aaf71ecb2b9d90dbfdff",
+    "url": "/ReactJS-Counter-Cart-Items/static/js/runtime~main.0f95ab8d.js"
+  },
+  {
+    "revision": "e939670671039530fbdf",
+    "url": "/ReactJS-Counter-Cart-Items/static/css/2.4cff009b.chunk.css"
+  },
+  {
+    "revision": "e939670671039530fbdf",
+    "url": "/ReactJS-Counter-Cart-Items/static/js/2.d0521b2a.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactJS-Counter-Cart-Items/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactJS-Counter-Cart-Items/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactJS-Counter-Cart-Items/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactJS-Counter-Cart-Items/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactJS-Counter-Cart-Items/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fbffff029ad24d481f031816e4fcb37f",
+    "url": "/ReactJS-Counter-Cart-Items/index.html"
+  }
+];
